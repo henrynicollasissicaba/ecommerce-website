efc62b02d033ba38e4d61f1b7bf9f0e347f4221c
Henry Nicollas <henrynissicaba@gmail.com>
2023-04-27 08:59:14 -0300

    remove js code not used

--- js/script.js
+++ js/script.js
@@ -43,8 +43,4 @@ var swiper = new Swiper(".product-slider", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
